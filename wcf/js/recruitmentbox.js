a6574b463f427d64741d0cf62cd323a4b1826598
Motrish <nehmer.michael@gmx.de>
2021-05-18 22:26:38 +0200

    Update recruitmentbox.js

--- wcf/js/recruitmentbox.js
+++ wcf/js/recruitmentbox.js
@@ -22,99 +22,45 @@ jQuery(document).ready
 		(
 			function()
 			{
-				if((document.getElementById("recruitback").style.backgroundImage).includes('horde_logo') == true){
+				if((document.getElementById("recruitback").style.backgroundImage).includes('horde_logo') == true){ var fraction = "horde"};
+				if((document.getElementById("recruitback").style.backgroundImage).includes('alliance_logo') == true){ var fraction = "alliance"};
+
+				if(document.documentElement.lang == "de"){ 
+					var text1 = 'Das Recruitement ist ge&oumlffnet.<br>Bewirb dich jetzt!';
+					var text2 = 'Anzahl der offenen Pl&aumltze: ';
+					var text3 = 'Das Recruitement f&uumlr diesen Spec ist derzeit geschlossen.';}
+				else if(document.documentElement.lang == "en"){ 
+					var text1 = 'The recruitment is open.<br>Apply now!';
+					var text2 = 'Free Slots for Class: ';
+					var text3 = 'The recruitment for this specialization is closed.';}
+				else if(document.documentElement.lang == "ru"){ 
+					var text1 = 'Набор открыт.<br>Присоедениться!';
+					var text2 = 'Кол-во свободных мест на класс: ';
+					var text3 = 'Набор персонажей данной специализации закрыт.';}					
+								
 				// Hover over code
 				var id = jQuery(this).parent().attr('id');
 				var className = jQuery(this).siblings(".className").text();
-				var classIcon = '<div class="specToolTipIcon horde"><img src=' + jQuery(this).siblings(".classIcon").children('img:first').attr('src') + '></div>';
-				var specIcon = '<div class="specToolTipIcon horde"><img src=' + jQuery(this).children('img:first').attr('src')+ '></div>';
-				var content = '<div class="specToolTipHead" id="' + id + '">' + jQuery(this).attr('title') + ' ' + className + '</div>';
+				var classIcon = '<div class="specToolTipIcon ' + fraction + '"><img src=' + jQuery(this).siblings(".classIcon").children('img:first').attr('src') + '></div>';
+				var specIcon = '<div class="specToolTipIcon ' + fraction +'"><img src=' + jQuery(this).children('img:first').attr('src')+ '></div>';
+				var content = '<div class="specToolTipHead" id="' + id + '">' + jQuery(this).attr('title') + ' - ' + className + '</div>';
 				content = content + specIcon + classIcon;
 				if (jQuery(this).children('.classSpecOverlay').attr('id') == 'active')
 				{
-					if(document.documentElement.lang == "de"){
-					content = content + '<div class="specToolTipDesc" id="text_active">Das Recruitement ist ge&oumlffnet.<br>Bewirb dich jetzt!</div>';
-					content = content + '<div class="specToolTipSlots" id="text_slots">Anzahl der offenen Pl&aumltze: ';
+					content = content + '<div class="specToolTipDesc" id="text_active">' + text1 + '</div>';
+					content = content + '<div class="specToolTipSlots" id="text_slots">' + text2 + '';
 					content = content + jQuery(this).children('.classSpecCounter').html() + '</div>';
-					}
-					else if(document.documentElement.lang == "en"){
-					content = content + '<div class="specToolTipDesc" id="text_active">The recruitment is open.<br>Apply now!</div>';
-					content = content + '<div class="specToolTipSlots" id="text_slots">Free Slots for Class: ';
-					content = content + jQuery(this).children('.classSpecCounter').html() + '</div>';
-					}
-					else{
-					content = content + '<div class="specToolTipDesc" id="text_active">The recruitment is open.<br>Apply now!</div>';
-					content = content + '<div class="specToolTipSlots" id="text_slots">Free Slots for Class: ';
-					content = content + jQuery(this).children('.classSpecCounter').html() + '</div>';
-					}
 				}
 				else
 				{
-					if(document.documentElement.lang == "de"){
-					content = content + '<div class="specToolTipDesc" id="text_inactive">Das Recruitement f&uumlr diesen Spec ist derzeit geschlossen.</div>';
-					}
-					else if(document.documentElement.lang == "en"){
-					content = content + '<div class="specToolTipDesc" id="text_inactive">The recruitment for this specialization is closed.</div>';
-					}
-					else{
-					content = content + '<div class="specToolTipDesc" id="text_inactive">The recruitment for this specialization is closed.</div>';
-					}	
+					content = content + '<div class="specToolTipDesc" id="text_inactive">' + text3 + '</div>';	
 				}
-				jQuery('<div class="specToolTip horde"></div>').html(content).appendTo('body').fadeIn('fast');
-
-
-
-				}
-				if((document.getElementById("recruitback").style.backgroundImage).includes('alliance_logo') == true){
-
-				
-				// Hover over code
-				var id = jQuery(this).parent().attr('id');
-				var className = jQuery(this).siblings(".className").text();
-				var classIcon = '<div class="specToolTipIcon alliance"><img src=' + jQuery(this).siblings(".classIcon").children('img:first').attr('src') + '></div>';
-				var specIcon = '<div class="specToolTipIcon alliance"><img src=' + jQuery(this).children('img:first').attr('src')+ '></div>';
-				var content = '<div class="specToolTipHead" id="' + id + '">' + jQuery(this).attr('title') + ' ' + className + '</div>';
-				content = content + specIcon + classIcon;
-				if (jQuery(this).children('.classSpecOverlay').attr('id') == 'active')
-				{
-					if(document.documentElement.lang == "de"){
-					content = content + '<div class="specToolTipDesc" id="text_active">Das Recruitement ist ge&oumlffnet.<br>Bewirb dich jetzt!</div>';
-					content = content + '<div class="specToolTipSlots" id="text_slots">Anzahl der offenen Pl&aumltze: ';
-					content = content + jQuery(this).children('.classSpecCounter').html() + '</div>';
-					}
-					else if(document.documentElement.lang == "en"){
-					content = content + '<div class="specToolTipDesc" id="text_active">The recruitment is open.<br>Apply now!</div>';
-					content = content + '<div class="specToolTipSlots" id="text_slots">Free Slots for Class: ';
-					content = content + jQuery(this).children('.classSpecCounter').html() + '</div>';
-					}
-					else{
-					content = content + '<div class="specToolTipDesc" id="text_active">The recruitment is open.<br>Apply now!</div>';
-					content = content + '<div class="specToolTipSlots" id="text_slots">Free Slots for Class: ';
-					content = content + jQuery(this).children('.classSpecCounter').html() + '</div>';
-					}
-				}
-				else
-				{
-					if(document.documentElement.lang == "de"){
-					content = content + '<div class="specToolTipDesc" id="text_inactive">Das Recruitement f&uumlr diesen Spec ist derzeit geschlossen.</div>';
-					}
-					else if(document.documentElement.lang == "en"){
-					content = content + '<div class="specToolTipDesc" id="text_inactive">The recruitment for this specialization is closed.</div>';
-					}
-					else{
-					content = content + '<div class="specToolTipDesc" id="text_inactive">The recruitment for this specialization is closed.</div>';
-					}	
-				}
-				jQuery('<div class="specToolTip alliance"></div>').html(content).appendTo('body').fadeIn('fast');
-
-				}
-
-
+				jQuery('<div class="specToolTip ' + fraction +'"></div>').html(content).appendTo('body').fadeIn('fast');
 			},
 			function()
 			{
 				// Hover out code
-				jQuery('.specToolTip').remove();
+				jQuery('.specToolTip').remove();	
 			}
 		)
 		.mousemove
